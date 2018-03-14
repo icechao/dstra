11dc4bfdc2277ec82f46489e37a0666bec3f44ca
Masao Hidemitsu <shirakox@protonmail.ch>
2018-03-14 19:37:38 +0900

    branding

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -50,12 +50,12 @@ bool AppInit(int argc, char* argv[])
         if (mapArgs.count("-?") || mapArgs.count("--help"))
         {
             // First part of help message is specific to bitcoind / RPC client
-            std::string strUsage = _("BlackCoin version") + " " + FormatFullVersion() + "\n\n" +
+            std::string strUsage = _("DSTRA version") + " " + FormatFullVersion() + "\n\n" +
                 _("Usage:") + "\n" +
-                  "  blackcoind [options]                     " + "\n" +
-                  "  blackcoind [options] <command> [params]  " + _("Send command to -server or blackcoind") + "\n" +
-                  "  blackcoind [options] help                " + _("List commands") + "\n" +
-                  "  blackcoind [options] help <command>      " + _("Get help for a command") + "\n";
+                  "  dstrad [options]                     " + "\n" +
+                  "  dstrad [options] <command> [params]  " + _("Send command to -server or dstrad") + "\n" +
+                  "  dstrad [options] help                " + _("List commands") + "\n" +
+                  "  dstrad [options] help <command>      " + _("Get help for a command") + "\n";
 
             strUsage += "\n" + HelpMessage();
 
@@ -65,7 +65,7 @@ bool AppInit(int argc, char* argv[])
 
         // Command-line RPC
         for (int i = 1; i < argc; i++)
-            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "blackcoin:"))
+            if (!IsSwitchChar(argv[i][0]) && !boost::algorithm::istarts_with(argv[i], "dstra:"))
                 fCommandLine = true;
 
         if (fCommandLine)
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BLK");
-    case mBTC: return QString("mBLK");
-    case uBTC: return QString::fromUtf8("μBLK");
+    case BTC: return QString("DST");
+    case mBTC: return QString("mDST");
+    case uBTC: return QString::fromUtf8("μDST");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BlackCoins");
-    case mBTC: return QString("Milli-BlackCoins (1 / 1,000)");
-    case uBTC: return QString("Micro-BlackCoins (1 / 1,000,000)");
+    case BTC: return QString("DSTRAs");
+    case mBTC: return QString("Milli-DSTRAs (1 / 1,000)");
+    case uBTC: return QString("Micro-DSTRAs (1 / 1,000,000)");
     default: return QString("???");
     }
 }
